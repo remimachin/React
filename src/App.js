1e0ebf2fa7ec0c1e60d09c3a0f28bfacf1db18d4
Prototype2000 <alphaproto2000@gmail.com>
2019-10-04 04:35:34 +0200

    Test react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import NavBar from './Project/Navbar'
+
+function App() {
+    return (
+        <div className="App">
+            <NavBar/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Project/Components/particle.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Particle extends Component {
+    render() {
+        return
+
+
+    }
+}
